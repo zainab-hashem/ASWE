9c20777b659f5ae1996ae334d49506650fa942ef
Zainab <s12218476@stu.najah.edu>
2026-03-27 00:21:45 +0200

    Add Route Estimation feature with OpenStreetMap integration

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const checkpointRoutes = require('./src/routes/checkpointRoutes');
 const incidentRoutes = require('./src/routes/incidentRoutes');  
 const authRoutes = require('./src/routes/authRoutes');
 const reportRoutes = require('./src/routes/reportRoutes');
+const mobilityRoutes = require('./src/routes/mobilityRoutes');
 
 app.use(express.json());
 
@@ -13,6 +14,7 @@ app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/reports', reportRoutes);
 app.use('/api/v1/checkpoints', checkpointRoutes);
 app.use('/api/v1/incidents', incidentRoutes);  
+app.use('/api/v1/mobility', mobilityRoutes);
 
 const PORT = 3000;
 
