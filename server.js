a89d41e8e010058bf5f80379e500106208ac31ef
diiinahanna2004 <dina.hanna2004@gmail.com>
2026-03-27 13:05:59 +0200

    add alert and subscription routes to server

--- server.js
+++ server.js
@@ -7,12 +7,20 @@ const incidentRoutes = require('./src/routes/incidentRoutes');
 const authRoutes = require('./src/routes/authRoutes');
 const reportRoutes = require('./src/routes/reportRoutes');
 
+//Feature4
+const alertRoutes = require('./src/routes/alertRoutes');
+const subscriptionRoutes = require('./src/routes/subscriptionRoutes');
+
 app.use(express.json());
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/reports', reportRoutes);
 app.use('/api/v1/checkpoints', checkpointRoutes);
-app.use('/api/v1/incidents', incidentRoutes);  
+app.use('/api/v1/incidents', incidentRoutes); 
+
+//Feature 4
+app.use('/api/v1/alerts', alertRoutes);
+app.use('/api/v1/subscriptions', subscriptionRoutes)
 
 const PORT = 3000;
 
